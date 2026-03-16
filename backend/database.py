875ec6924b87906f88424513ca84cc57bb05bd0c
Soham Deshpande <ssdeshp2@uci.edu>
2026-03-15 18:39:00 -0700

    Implement database.py -- engine, Base, and async connection

---
+++ backend/database.py
@@ -0,0 +1,20 @@
+# Database initialization for ics-rack-demo
+
+# Imports
+
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from databases import Database
+
+
+# Load .env variables
+load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+
+# Create database entry point
+engine = create_engine(DATABASE_URL, connect_args={'check_same_thread' : False} )
+Base = declarative_base()
+database = Database(DATABASE_URL)
